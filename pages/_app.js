f84d5f9f45312fd1139db9e61f8a6cc541095282
Lofton <cary.l.gentry.jr@gmail.com>
2023-04-14 22:57:02 -0400

    Removed chakra provided and added app wrapper

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,21 @@
 import Layout from '@/Components/Layout'
 import { Poppins } from 'next/font/google'
-import { ChakraProvider } from '@chakra-ui/react'
+import AppWrapper from '@/Components/AppWrapper'
 import '@/styles/globals.css'
 
-const poppins = Poppins({ 
+const poppins = Poppins({
   subsets: ['latin'],
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
 })
 
 export default function App({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <main className={poppins.className}>
+    <main className={poppins.className}>
+      <AppWrapper>
         <Layout>
           <Component {...pageProps} />
         </Layout>
-      </main>
-    </ChakraProvider>
+      </AppWrapper>
+    </main>
   )
 }
