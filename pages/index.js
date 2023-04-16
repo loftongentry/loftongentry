ddcb22a2d1dc1bab7e6e0829f73a3e95bba2b784
Lofton <cary.l.gentry.jr@gmail.com>
2023-04-15 22:29:37 -0400

    Adding Glassmorphism card to home page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
+
+import GlassCard from '@/Components/GlassCard'
 import styles from '../styles/Index.module.css'
+
 
 export default function Home() {
   return (
     <div className={styles.content}>
-      <div className={styles.blob}></div>
+      <h1>Welcome!</h1>
+      <div className={styles.cards}>
+        <GlassCard
+          cardHeader='Card 1'
+          cardContent='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+          cardLink='Link Here'
+          cardLinkTitle='Click Here'
+        ></GlassCard>
+      </div>
     </div>
   )
 }
