706b8ea87affc6fa9be16bec8ec050eabe416d7b
Lofton <cary.l.gentry.jr@gmail.com>
2023-04-17 18:43:49 -0400

    Adjusted cardContent, added more details

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
-import GlassCard from '@/Components/Page Components/GlassCard'
-import styles from '../styles/Page Styles/Index.module.css'
+import GlassCard from '@/Components/Page Components/Index Components/GlassCard'
 import Divider from '@/Components/Page Components/Divider'
-import GlassSkillCard from '@/Components/Page Components/GlassSkillCard'
-
+import SkillCard from '@/Components/Page Components/Index Components/SkillCard'
+import styles from '../styles/Page Styles/Index.module.css'
+import { DiReact, DiCss3, DiJavascript, DiPython, DiJava } from 'react-icons/di'
+import { AiFillHtml5, AiFillGithub } from 'react-icons/ai'
+import { FcGoogle } from 'react-icons/fc'
+import { BsGit } from 'react-icons/bs'
+import { SiPlanetscale, SiChakraui } from 'react-icons/si'
 
 export default function Home() {
   return (
@@ -24,35 +28,31 @@ export default function Home() {
       <div className={styles.cards}>
         <GlassCard
           cardHeader='Fruity Shooty'
-          cardContent='This was a game a simple game I designed to gain more experience programming in general'
+          cardContent='This was a simple game I made to gain more experience with programming in general'
           buttonTo={false}
           gitHubLink='https://github.com/loftongentry/Fruity-Shooty'
         />
         <GlassCard
           cardHeader='Word Bag Extraction'
-          cardContent='This was a program I developed for the government that displays the number of words that appear in a PDF document'
+          cardContent='This is a simple program I developed for the US government that displays the number of words that appear in a PDF document on a webpage'
           buttonTo={false}
           gitHubLink='https://github.com/loftongentry/Word-Bag-Extraction'
         />
       </div>
       <Divider title='My Skills' />
       <div className={styles.skillCards}>
-        <GlassSkillCard
+        <SkillCard
           cardHeader='Proficent With'
-          cardContent='This is the card body'
+          cardContent={['React', 'JavaScript', 'HTML', 'CSS', 'Google App Script', 'GitHub']}
+          tag={[DiReact, DiJavascript, AiFillHtml5, DiCss3, FcGoogle, AiFillGithub]}
+          tagStyle={['#61DAFB', '#F7DF1E', '#E34F26', '#1572B6', '', '#181717']}
         />
-        <GlassSkillCard
+        <SkillCard
           cardHeader='Experience With'
-          cardContent='This is the card body'
+          cardContent={['Chakra UI', 'Planetscale', 'Git', 'Python', 'Java']}
+          tag={[SiChakraui, SiPlanetscale, BsGit, DiPython, DiJava]}
+          tagStyle={['#319795', '#000000', '#F05032', '#3776AB', '']}
         />
-        <GlassSkillCard
-          cardHeader='Exposure To'
-          cardContent='This is the card body'
-        />
-      </div>
-      {/* This button is just for testing purposes */}
-      <div className={styles.buttonWrapper}>
-        <p className={styles.button}>This is going to be a stylish button</p>
       </div>
       <Divider />
     </div>
