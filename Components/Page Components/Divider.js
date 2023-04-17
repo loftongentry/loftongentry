a2e2f605368d363cbac2ff52329d2f9a6776d279
Lofton <cary.l.gentry.jr@gmail.com>
2023-04-16 21:34:10 -0400

    Changed layout of divider

--- Components/Page Components/Divider.js
+++ Components/Page Components/Divider.js
@@ -3,8 +3,8 @@ import styles from '../../styles/Page Component Styles/Divider.module.css'
 const Divider = ({title}) => {
   return (
     <div className={styles.container}>
-      <hr className={styles.divider}/>
       <h3 className={styles.secondaryHeader}>{title}</h3>
+      <hr className={styles.divider}/>
     </div>
   )
 }
