d40f23608b89f0e7202915973ace847ad1838db0
Lofton <cary.l.gentry.jr@gmail.com>
2023-04-21 13:39:26 -0400

    Removed TextArea requirement to send email

--- Components/Page Components/Index Components/TextArea.js
+++ Components/Page Components/Index Components/TextArea.js
@@ -1,25 +1,16 @@
-import { useState } from 'react'
 import styles from '../../../styles/Page Component Styles/Index Styles/TextArea.module.css'
 
 const TextArea = (props) => {
-  const [hasError, setHasError] = useState(false)
-
-  const handleBlur = () => {
-    !props.value ? setHasError(true) : setHasError(false)
-  }
-
   return (
     <div className={styles.textAreaContainer}>
       <label className={styles.textAreaLabel}>{props.label}</label>
       <textarea
-        className={hasError ? styles.textAreaError : styles.textArea}
+        className={styles.textArea}
         name={props.name}
         value={props.value}
         placeholder={props.placeholder}
         onChange={props.onChange}
-        onBlur={handleBlur}
       ></textarea>
-      {hasError && <p className={styles.errorMessage}>Required</p>}
     </div>
   )
 }
