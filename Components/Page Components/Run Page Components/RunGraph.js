dbb4f54e43d7eda1b97bf043190fff3a9ed12132
Lofton <cary.l.gentry.jr@gmail.com>
2023-05-07 22:51:38 -0400

    Adding comments

--- Components/Page Components/Run Page Components/RunGraph.js
+++ Components/Page Components/Run Page Components/RunGraph.js
@@ -2,9 +2,11 @@ import { useEffect, useRef } from "react"
 import * as d3 from 'd3'
 
 const RunGraph = ({ data }) => {
+  
   const svgRef = useRef()
 
   useEffect(() => {
+    //Taking the runData returned from MongoDB and creating a new data object that is better for displaying my data on a graph
     const chartData = data.map(item => ({
       date: item.date,
       runTime: item.runTime,
@@ -15,17 +17,23 @@ const RunGraph = ({ data }) => {
       totalCalories: item.totalCalories
     }))
 
+    //Parameters of the size of the graph
     const margin = { top: 20, right: 20, bottom: 30, left: 50 }
     const width = 960 - margin.left - margin.right
     const height = 500 - margin.top - margin.bottom
 
     const svg = d3.select(svgRef.current)
 
-    const x = d3.scaleTime().range([0, width]).domain(d3.extent(chartData, d => d.date))
-
+    //Create a new element with a new time scale based on helper functions
+    const x = d3.scaleTime()
+      //Returns an array of evenly spaced numbers from the first element array to the last element in the array minus 1
+      .range([0, width])
+      //Sets the domain of the time scale to the earliest data and most recent date
+      .domain(d3.extent(chartData, d => d.date))
+      //Make sure that the start and end are on round date values
+      .nice()
+    
     const y = d3.scaleLinear().range([height, 0])
-
-    const line = d3.line().x(d => x(d.date)).y(d => y(d.value))
 
     const values = Object.keys(chartData[0]).filter((key) => key !== "date");
 
@@ -33,6 +41,10 @@ const RunGraph = ({ data }) => {
       d3.min(chartData, d => d3.min(values, value => d[value])),
       d3.max(chartData, d => d3.max(values, value => d[value]))
     ])
+
+    const line = d3.line()
+      .x(d => x(d.date))
+      .y(d => values?.map(value => y(d[value])))
 
     const paths = values?.map(value => {
       return {
