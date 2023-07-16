1f248411b8784da8d94bd667764d7476065e18ff
Lofton <cary.l.gentry.jr@gmail.com>
2023-07-16 19:32:23 -0400

    Removed loader and put in preLoader file

--- Components/PreLoader/PreLoader.js
+++ Components/PreLoader/PreLoader.js
@@ -1,19 +1,65 @@
-//TODO: Remove artifact
 import { useEffect } from "react"
-import { preLoaderAnim } from "./Loader"
 import { gsap } from "gsap"
 import { preLoaderStyle, textContainerStyle, textWrapperStyle, spanColumnStyle, spanStyle } from "./PreLoaderStyles"
 
 const PreLoader = () => {
   useEffect(() => {
+    let disableScroll = () => {
+      window.addEventListener('scroll', preventScroll, { passive: false })
+    }
+
+    let preventScroll = (event) => {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+
+    let enableScrolling = () => {
+      window.removeEventListener('scroll', preventScroll)
+    }
+
+    disableScroll()
+
+    const tl = gsap.timeline()
+
     let ctx = gsap.context(() => {
-      preLoaderAnim()
+      gsap.set('.span-col span', {
+        opacity: '0'
+      })
+
+      tl.to('.span-col span', {
+        opacity: '1',
+        y: '20',
+        stagger: '0.5',
+        delay: 1.5
+      })
+        .to('.span-col', {
+          y: '100%',
+          duration: 1,
+          ease: 'power3.inOut',
+          stagger: 0.5,
+          delay: 1
+        })
+        .to('.text-wrapper', {
+          y: '100%',
+          duration: 0.5,
+          ease: 'power3.inOut',
+        })
+        .to('.pre-loader', {
+          opacity: 0,
+          transformOrigin: 'bottom',
+          duration: 0,
+          onComplete: enableScrolling
+        });
     })
-    return () => ctx.revert()
+
+    return () => {
+      enableScrolling()
+      ctx.revert()
+    }
   }, [])
 
   return (
-    <div className="pre-loader" style={preLoaderStyle}>
+    <div className="pre-loader" style={{...preLoaderStyle, pointerEvents: "none"}}>
       <div className="text-wrapper" style={textWrapperStyle}>
         <div className="text-container" style={textContainerStyle}>
           <div className='span-col' style={spanColumnStyle}>
