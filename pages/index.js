790f59ea050cfa74384cf71319c96c26091eed54
Lofton <cary.l.gentry.jr@gmail.com>
2023-07-30 11:20:19 -0400

    Added new skills

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,11 @@ import GlassCard from '@/Components/Page Components/Index Components/GlassCard'
 import Divider from '@/Components/Global Components/Divider'
 import SkillCard from '@/Components/Page Components/Index Components/SkillCard'
 import Form from '@/Components/Form Components/Form'
-import { DiReact, DiCss3, DiJavascript, DiPython, DiJava } from 'react-icons/di'
-import { AiFillHtml5, AiFillGithub } from 'react-icons/ai'
+import { DiReact, DiCss3, DiJavascript, DiPython, DiJava, DiPostgresql, DiMongodb, DiGoogleCloudPlatform } from 'react-icons/di'
+import { AiFillHtml5 } from 'react-icons/ai'
 import { FcGoogle } from 'react-icons/fc'
 import { BsGit } from 'react-icons/bs'
-import { SiPlanetscale, SiChakraui } from 'react-icons/si'
+import { SiPlanetscale } from 'react-icons/si'
 import { sendContactForm } from '@/library/sendContactForm'
 import styles from '@/styles/Page Styles/Index.module.css'
 import buttonStyles from '@/styles/Global Component Styles/Button.module.css'
@@ -37,8 +37,8 @@ export default function Home() {
         />
         <GlassCard
           cardHeader={`Portfolio Website`}
-          languages='React, HTML, CSS'
-          timeline='Apr. 2023'
+          languages='React, HTML, CSS, MongoDB'
+          timeline='Apr. 2023 - July 2023'
           cardContent='The current website that you are on. This website is mainly used for
           demonstrating my growing skills with front-end programming'
           buttonTo={false}
@@ -51,7 +51,7 @@ export default function Home() {
           cardHeader='Fruity Shooty'
           languages='GML'
           timeline='Oct. 2020'
-          cardContent='This was a simple game I made to gain more experience with programming in general'
+          cardContent='This was a simple I made to gain more experience with programming in general'
           buttonTo={false}
           gitHubLink='https://github.com/loftongentry/Fruity-Shooty'
         />
@@ -59,7 +59,7 @@ export default function Home() {
           cardHeader='Word Bag Extraction'
           languages='Python'
           timeline='Summer 2019'
-          cardContent='This is a simple program I developed for the US government that displays the number of words that appear in a PDF document on a webpage'
+          cardContent='This is a program I developed for the US government that displays the number of words that appear in a PDF document on a webpage'
           buttonTo={false}
           gitHubLink='https://github.com/loftongentry/Word-Bag-Extraction'
         />
@@ -68,21 +68,21 @@ export default function Home() {
       <div className={styles.skillCards}>
         <SkillCard
           cardHeader='Proficent With'
-          cardContent={['React', 'JavaScript', 'HTML', 'CSS', 'Google App Script', 'GitHub']}
-          tag={[DiReact, DiJavascript, AiFillHtml5, DiCss3, FcGoogle, AiFillGithub]}
-          tagStyle={['#61DAFB', '#F7DF1E', '#E34F26', '#1572B6', '', '#181717']}
+          cardContent={['React', 'JavaScript', 'HTML', 'CSS', 'Google App Script',]}
+          tag={[DiReact, DiJavascript, AiFillHtml5, DiCss3, FcGoogle]}
+          tagStyle={['#61DAFB', '#F7DF1E', '#E34F26', '#1572B6', '']}
         />
         <SkillCard
           cardHeader='Experience With'
-          cardContent={['Chakra UI', 'Git', 'Python', 'Java']}
-          tag={[SiChakraui, BsGit, DiPython, DiJava]}
-          tagStyle={['#319795', '#F05032', '#3776AB', '']}
+          cardContent={['PostgreSQL', 'Git', 'Python', 'Java']}
+          tag={[DiPostgresql, BsGit, DiPython, DiJava]}
+          tagStyle={[' #0064a5', '#F05032', '#3776AB', '']}
         />
         <SkillCard
           cardHeader='Exposure To'
-          cardContent={['Planetscale']}
-          tag={[SiPlanetscale]}
-          tagStyle={['#000000']}
+          cardContent={['MongoDB', 'Planetscale', 'Google Cloud']}
+          tag={[DiMongodb, SiPlanetscale, DiGoogleCloudPlatform]}
+          tagStyle={['#589636', '#000000', '#4285F4']}
         />
       </div>
       <Divider />
